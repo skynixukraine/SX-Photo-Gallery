31c6e7523db1a73b93c0be7cdac1a51380c5c809
Volodymir <volodymyr.babak@skynix.co>
2018-02-12 16:36:33 +0200

    SPG-2 - pre-finish posts and galleries functionality

--- classes/class-sxpg-init.php
+++ classes/class-sxpg-init.php
@@ -11,14 +11,14 @@ class SXPG_init {
     }
 
     public function init() {
-        // Register new post type
-        add_action( 'init', array( $this->gallery, 'sxpg_create_post_type' ) );
+        $this->gallery->init();
         // Register settings options
         add_action( 'admin_init', array( $this->settings, 'sxpg_settings_fields' ) );
         // Create plugin menu item
         add_action( 'admin_menu', array( $this, 'sxpg_add_menu_page' ) );
         // Create settings subpage
         add_action( 'admin_menu', array( $this->settings, 'sxpg_options_page' ) );
+
     }
 
     /**
