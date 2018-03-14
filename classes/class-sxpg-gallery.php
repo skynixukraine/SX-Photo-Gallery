2df9d9c9f820ab4e6a07b53b639b1dacdd37a784
Volodymir <volodymyr.babak@skynix.co>
2018-03-14 17:41:39 +0200

    SPG-5 - do not display image if it is not set

--- classes/class-sxpg-gallery.php
+++ classes/class-sxpg-gallery.php
@@ -247,13 +247,17 @@ class SXPG_gallery {
 
             while ( $gallery->have_posts() ) {
                 $gallery->the_post();
-                $attachment_id = get_post_thumbnail_id( $gallery->post->ID );
 
-                $response .= '<li class="sx-photo-gallery-photos__photo-container">';
-                $response .= '<a href="' . get_permalink( $gallery->post->ID ) . '">';
-                $response .= '<img class="sx-photo-gallery-photos__photo" src="' . wp_get_attachment_image_src( $attachment_id, 'full' )[0] . '" alt="' . $gallery->post->post_title . '" />';
-                $response .= '</a>';
-                $response .= '</li>';
+                $attachment_id = get_post_thumbnail_id( $gallery->post->ID );
+                $image_src     = wp_get_attachment_image_src( $attachment_id, 'full' )[0];
+
+                if ( !empty( $image_src ) ) {
+                    $response .= '<li class="sx-photo-gallery-photos__photo-container">';
+                    $response .= '<a href="' . get_permalink($gallery->post->ID) . '">';
+                    $response .= '<img class="sx-photo-gallery-photos__photo" src="' . $image_src . '" alt="' . $gallery->post->post_title . '" title="' . $gallery->post->post_title . '" />';
+                    $response .= '</a>';
+                    $response .= '</li>';
+                }
             }
 
             $response .= '</ul>';
